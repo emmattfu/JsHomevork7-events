f6a97a6c04015ab5eb3d13b337881deac6cc507b
emmattfu <makcimdjan@gmail.com>
2018-06-02 09:19:59 +0300

    3

--- js/plugin.js
+++ js/plugin.js
@@ -25,6 +25,52 @@ function removeColor(evn) {
 
 function bodyClick(evn) {
     if (paragraph.lastChild.data) paragraph.lastChild.data = '';
-    let tagName = evn.target.tagName;
+    let tagName = evn.target.tagName.toLowerCase();
     paragraph.insertAdjacentText("beforeend", tagName);
+}
+
+let arr = [
+    {
+        "лето": "одень футболку"
+    },
+    {
+        "осень": "одень куртку и штаны"
+    },
+    {
+        "зима": "одень шубу и теплые штаны"
+    },
+    {
+        "весна": "одень ветровку и джинсы"
+    }
+];
+const form = document.forms[0];
+const select = form.elements[0];
+const input = form.elements[1];
+select.addEventListener('change', formFunc);
+
+function formFunc(evn) {
+    input.value = handler(arr);
+}
+
+function handler(array, callback) {
+    for (let element of arr) {
+        if (select.value.toLowerCase() === handler2(element)) return handler3(element);
+    }
+    // return result;
+}
+
+function handler2(element) {
+    let result;
+    for (let value in element) {
+         result = value;
+    }
+    return result;
+}
+
+function handler3(element) {
+    let result;
+    for (let value in element) {
+        result = element[value];
+    }
+    return result;
 }
